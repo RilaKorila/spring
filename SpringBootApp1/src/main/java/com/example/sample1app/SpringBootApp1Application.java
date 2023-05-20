c99f09eae80d3c3deb06edc784b350b759e1d205
RilaKorila <murakami.ayana3@gmail.com>
2023-05-21 08:44:06 +0900

    chap 2-2: CommandLineRunner

--- SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
+++ SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
@@ -1,20 +1,30 @@
 package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
-public class SpringBootApp1Application {
+// @RestController
+public class SpringBootApp1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp1Application.class, args);
+		SpringApplication app = new SpringApplication(SpringBootApp1Application.class);
+		app.run(args);
 	}
 
-	@RequestMapping("/")
-	public String index(){
-		return "Hello, Spring boot3!";
+	// @RequestMapping("/")
+	// public String index(){
+	// 	return "Hello, Spring boot3!";
+	// }
+
+	@Override
+	public void run(String[] args) {
+		System.out.println("+----------------------------------+");
+		System.out.println("|This is CommandLine Runner Program|");
+		System.out.println("+----------------------------------+");
+		
 	}
 }
