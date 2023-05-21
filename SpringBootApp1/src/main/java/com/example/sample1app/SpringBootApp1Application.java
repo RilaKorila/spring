dff72c3760d73b2f3150455d3d3181dc280f027e
RilaKorila <murakami.ayana3@gmail.com>
2023-05-21 10:15:47 +0900

    2-3: return DataObject as JSON

--- SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
+++ SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
@@ -1,40 +1,75 @@
 package com.example.sample1app;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @SpringBootApplication
-// @RestController
-public class SpringBootApp1Application implements CommandLineRunner {
+@RestController
+public class SpringBootApp1Application{
+	final DataObject[] data = {
+			new DataObject("noname", "no email adress", 0),
+			new DataObject("taro", "taro@gmail.com", 35),
+			new DataObject("hanako", "hanako@gmail.com", 23),
+			new DataObject("jiro", "jiro@gmail.com", 10),
+	};
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringBootApp1Application.class);
-		app.setHeadless(false);
 		app.run(args);
 	}
 
-	// @RequestMapping("/")
-	// public String index(){
-	// 	return "Hello, Spring boot3!";
-	// }
+	 @RequestMapping("/")
+	 public String index(){
+	 	return "Hello, Spring boot3!";
+	 }
 
-	@Override
-	public void run(String[] args) {
-		JFrame frame = new JFrame("Spring Boot Swing App");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 200);
-		frame.add(new JLabel("Spring Boot Application!"));
-		frame.setVisible(true);
+	 @RequestMapping("/{num}")
+	 public DataObject index(@PathVariable int num){
+		int n  = num < 0 ? 0: num > data.length ? 0: num;
+		return data[n];
+	 }
+}
 
-		System.out.println("+----------------------------------+");
-		System.out.println("|This is CommandLine Runner Program|");
-		System.out.println("+----------------------------------+");
-		
+class DataObject{
+	String name;
+	String mail;
+	int age;
+
+	public DataObject(String name, String mail, int age){
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.age = age;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String newName) {
+		this.name = newName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(final String newMail) {
+		this.mail = newMail;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(final int newAge) {
+		this.age = newAge;
+	}
+
+
 }
