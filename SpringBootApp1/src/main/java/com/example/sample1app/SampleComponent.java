6cec0ef56190e49b91b215e27870c031bbf92eed
RilaKorila <murakami.ayana3@gmail.com>
2023-06-04 08:27:40 +0900

    chap 7-1: アプリケーションプロパティを利用する

--- SpringBootApp1/src/main/java/com/example/sample1app/SampleComponent.java
+++ SpringBootApp1/src/main/java/com/example/sample1app/SampleComponent.java
@@ -1,10 +1,16 @@
 package com.example.sample1app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleComponent {
-    private String message = "default message";
+    // private String message = "default message";
+
+    // 引数でmessageを指定
+    // プロパティファイルがUTF-8エンコーディングじゃないので文字化けするが、本質じゃないので一旦放置
+    @Value("${sample1app.samplecomponent.message:default value}")
+    private String message;
 
     public SampleComponent(){
         super();
