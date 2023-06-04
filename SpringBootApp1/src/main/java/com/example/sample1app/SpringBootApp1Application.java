bc30fe3027026a8835e11537ddbad0fca67cc527
RilaKorila <murakami.ayana3@gmail.com>
2023-06-04 10:22:49 +0900

    chap 7-1: 構成クラスの設定

--- SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
+++ SpringBootApp1/src/main/java/com/example/sample1app/SpringBootApp1Application.java
@@ -21,16 +21,6 @@ public class SpringBootApp1Application{
 			new DataObject("jiro", "jiro@gmail.com", 10),
 	};
 
-	@Bean
-	public Post post(){
-		// returnするインスタンスをbeanとして登録
-		return new Post(0, 0, "dummy", "ダミーだよ");
-	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder){
-		return builder.build();
-	}
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringBootApp1Application.class);
 		app.run(args);
