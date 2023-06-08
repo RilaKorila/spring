d2dc016d97a358793729dc25e2df191ca8e89d7a
RilaKorila <murakami.ayana3@gmail.com>
2023-06-08 09:44:56 +0900

    kotlin app: addページを追加

--- kotolinapp/src/main/kotlin/com/example/kotolinapp/MainController.kt
+++ kotolinapp/src/main/kotlin/com/example/kotolinapp/MainController.kt
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
@@ -22,6 +24,18 @@ class MainController {
 //        mav.addObject("title", "メインページ");
 //        mav.addObject("users", userRepository.findAll());
 //        return mav;
+    }
+
+    @GetMapping("/add")
+    fun add(model: Model):String{
+        return "add"
+    }
+
+    @PostMapping("/add")
+    fun addUser(@RequestParam name: String): String{
+        // Userの第1引数に既存のidを渡すとupdateになる
+        userRepository.save(User(0, name));
+        return "redirect:/"
     }
 
 }
